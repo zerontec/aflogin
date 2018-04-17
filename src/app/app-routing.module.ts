b6a8de1793c65b6f6b290de15183f0a54e0e2e02
zerontec <zerontecnologia1@gmail.com>
2018-04-17 16:46:46 -0400

    navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './componentes/home-page/home-page.component';
+import {LoginPageComponent} from './componentes/login-page/login-page.component';
+import {RegisterPageComponent} from './componentes/register-page/register-page.component';
+import {PrivatePageComponent} from './componentes/private-page/private-page.component';
+import {NoFoundPageComponent } from './componentes/no-found-page/no-found-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'private', component: PrivatePageComponent},
+  {path: '**', component: NoFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
