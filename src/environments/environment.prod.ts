ddd6659f1bda6debc4f7f93e76065c19410c8323
zerontec <zerontecnologia1@gmail.com>
2018-04-18 15:53:43 -0400

    configuracion a firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{ apiKey: 'AIzaSyAp3lUFv9BwVVJclAKg8z8dLIfHw6rUuOQ',
+  authDomain: 'angucf-fd940.firebaseapp.com',
+  databaseURL: 'https://angucf-fd940.firebaseio.com',
+  projectId: 'angucf-fd940',
+  storageBucket: 'angucf-fd940.appspot.com',
+  messagingSenderId: '964287523892'
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+ firebaseConfig: { apiKey: 'AIzaSyAp3lUFv9BwVVJclAKg8z8dLIfHw6rUuOQ',
+  authDomain: 'angucf-fd940.firebaseapp.com',
+  databaseURL: 'https://angucf-fd940.firebaseio.com',
+  projectId: 'angucf-fd940',
+  storageBucket: 'angucf-fd940.appspot.com',
+  messagingSenderId: '964287523892'
+}
 };
